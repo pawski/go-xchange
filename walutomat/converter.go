052bfd8f0eba337705f683074a92f5d29ed43776
Pawel Czyzewski <pawel.czyzewski@enginewerk.com>
2019-01-17 21:48:49 +0100

    Json field type fix 2

--- walutomat/converter.go
+++ walutomat/converter.go
@@ -1,12 +1,12 @@
 package walutomat
 
 import (
+	"strconv"
 	"encoding/json"
 	"github.com/pawski/go-xchange/logger"
-	"strconv"
 )
 
-func Convert(response []byte) []Offer {
+func Convert(response []byte) ([] Offer) {
 	var offerResponseDecoded OffersResponse
 
 	err := json.Unmarshal(response, &offerResponseDecoded)
@@ -15,43 +15,38 @@ func Convert(response []byte) []Offer {
 		logger.Get().Fatal(err)
 	}
 
-	offers := offerResponseToOffer(offerResponseDecoded)
+	offers :=  offerResponseToOffer(offerResponseDecoded)
 
-	logger.Get().Debugf("Offers in response: %d", len(offers))
+	logger.Get().Printf("Offers in response: %d", len(offers))
 
 	return offers
 }
 
-func offerResponseToOffer(offerResponse OffersResponse) []Offer {
+func offerResponseToOffer(offerResponse OffersResponse) ([] Offer) {
 	var offers []Offer
 
 	for _, offerResponse := range offerResponse.Offers {
 		offers = append(offers, Offer{
-			Pair:      offerResponse.Pair,
-			Buy:       stringToFloat(offerResponse.Buy.String()),
-			BuyOld:    stringToFloat(offerResponse.BuyOld.String()),
-			Sell:      stringToFloat(offerResponse.Sell.String()),
-			SellOld:   stringToFloat(offerResponse.SellOld.String()),
-			CountBuy:  offerResponse.CountBuy,
+			Pair: offerResponse.Pair,
+			Buy: offerResponse.Buy,
+			BuyOld: offerResponse.BuyOld,
+			Sell: offerResponse.Sell,
+			SellOld: offerResponse.SellOld,
+			CountBuy: offerResponse.CountBuy,
 			CountSell: offerResponse.CountSell,
-			Avg:       stringToFloat(offerResponse.Avg.String()),
-			AvgOld:    stringToFloat(offerResponse.AvgOld.String()),
+			Avg: offerResponse.Avg,
+			AvgOld: offerResponse.AvgOld,
 		})
 	}
 
 	return offers
 }
 
-func stringToFloat(floatAsString string) float32 {
-	if floatAsString == "" {
-		return 0.0
-	}
-
+func stringToFloat(floatAsString string) (float32)  {
 	result, err := strconv.ParseFloat(floatAsString, 32)
 
 	if nil != err {
-		logger.Get().Errorf("Conversion error %s\n", err)
-		return 0.0
+		logger.Get().Printf("Conversion error %s\n", err)
 	}
 
 	return float32(result)
