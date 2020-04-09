4dfd8ccbdaf0150608d72a903f5ced66c878a79a
Pawel Czyzewski <pawel.czyzewski@enginewerk.com>
2020-04-09 15:33:59 +0200

    Extended currency pairs

--- walutomat/currencyPair.go
+++ walutomat/currencyPair.go
@@ -3,5 +3,14 @@ package walutomat
 type CurrencyPair string
 
 const (
+	EURGBP CurrencyPair = "EURGBP"
+	EURUSD CurrencyPair = "EURUSD"
+	EURCHF CurrencyPair = "EURCHF"
 	EURPLN CurrencyPair = "EURPLN"
+	GBPUSD CurrencyPair = "GBPUSD"
+	GBPCHF CurrencyPair = "GBPCHF"
+	GBPPLN CurrencyPair = "GBPPLN"
+	USDCHF CurrencyPair = "USDCHF"
+	USDPLN CurrencyPair = "USDPLN"
+	CHFPLN CurrencyPair = "CHFPLN"
 )
